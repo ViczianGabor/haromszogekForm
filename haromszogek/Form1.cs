1f1443f928144649f38cc81ff46156fc3c2a2449
ViczianGabor <viczian.gabor13@gmail.com>
2020-10-05 12:38:00 +0200

    Add project files.

---
+++ haromszogek/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace haromszogek
+{
+    public partial class frmFo : Form
+    {
+
+        private int Aoldal;
+        private int Boldal;
+        private int Coldal;
+
+        public frmFo()
+        {
+            Aoldal = 0;
+            Boldal = 0;
+            Coldal = 0;
+            InitializeComponent();
+            tbAoldal.Text = Aoldal.ToString();
+            tbBoldal.Text = Boldal.ToString();
+            tbColdal.Text = Coldal.ToString();
+            lbHaromsogLista.Items.Clear();
+
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+
+
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            if (lbHaromsogLista.Items.Count >0)
+            {
+                lbHaromsogLista.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Nincs mit törölni");
+            }
+            
+
+            
+        }
+
+        private void btnSzamol_Click(object sender, EventArgs e)
+        {
+            Aoldal = int.Parse(tbAoldal.Text);
+            Boldal = int.Parse(tbBoldal.Text);
+            Coldal = int.Parse(tbColdal.Text);
+
+
+            if (Aoldal == 0 || Boldal == 0 || Coldal == 0)
+            {
+                MessageBox.Show("Ez nem lehet nulla", "Ez most", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var h = new Haromszog(Aoldal, Boldal, Coldal);
+                if (h.szerkesztheto)
+                {
+                    MessageBox.Show("Kerület: " + h.kerulet + "Terület: " + h.terulet);
+                }
+                else
+                {
+                    MessageBox.Show("Nem szerkeszthető belőle háromszög");
+                }
+
+
+                List<string> adatok = h.adatokSzoveg();
+                foreach (var a in adatok)
+                {
+                    lbHaromsogLista.Items.Add(a);
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ haromszogek/Haromszog.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace haromszogek
+
+{
+    internal class Haromszog
+    {
+        private int aoldal;
+        private int boldal;
+        private int coldal;
+
+        public Haromszog(int aoldal, int boldal, int coldal)
+        {
+            this.aoldal = aoldal;
+            this.boldal = boldal;
+            this.coldal = coldal;
+            szerk();
+        }
+
+        public double terulet { get; private set; }
+
+        public double kerulet { get; private set; }
+
+
+        public bool szerkesztheto { get; private set; }
+
+
+        private void szerk()
+        {
+            if (aoldal + boldal > coldal &&
+                boldal + coldal > aoldal &&
+                aoldal + coldal > boldal
+                )
+
+            {
+                szerkesztheto = true;
+                terulet = teruletszamitas();
+                kerulet = keruletszamitas();
+            }
+            else
+            {
+                szerkesztheto = false;
+                terulet = 0;
+                kerulet = 0;
+            }
+
+
+        }
+
+        private double teruletszamitas()
+        {
+            double s = 0;
+            s += aoldal + boldal + coldal / 2;
+            return Math.Sqrt(s * (s - aoldal) * (s - boldal) * (s - coldal));
+
+
+
+
+        }
+
+
+        private double keruletszamitas()
+        {
+            return aoldal + boldal + coldal;
+
+
+        }
+
+
+        public List<string> adatokSzoveg()
+        {
+            List<string> adatok = new List<string>();
+            
+            
+            if (szerkesztheto)
+            {
+                adatok.Add($"a: {aoldal} - b: {boldal} - c: {coldal}");
+            }
+            else
+            {
+                adatok.Add("Nem szerkeszthető");
+            }
+            return adatok;
+        }
+
+
+
+    }
+}
